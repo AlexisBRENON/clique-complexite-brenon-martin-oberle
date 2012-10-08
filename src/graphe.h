36e6f873476c702b64d2e9f03db7a117fec4917a
brenon.alexis <brenon.alexis@0f5846b4-0016-f348-011b-61e5118c5e03>
2012-10-08 13:49:37 +0000

    Ajout des principaux entetes de fonctions sur les graphes. Manque Les trois derniere du sujet. Doc Doxygen a vérifier.

--- src/graphe.h
+++ src/graphe.h
@@ -1,44 +1,139 @@
-#ifndef STRUCT_H
-#define STRUCT_H
-#define n_max 10 /*nombre maximum de sommets*/
-typedef int sommet; /*indices des sommets*/
+#ifndef __GRAPHE_H__
+#define __GRAPHE_H__
 
-/*representation par matrice d'adjacence*/
-typedef struct
-{ 
-	int a[n_max][n_max]; /*matrice carre representant les valeurs de c entre 2sommets*/
-	int circ[n_max][n_max]; /*matrice carre representant les valeurs circ entre 2sommets*/
-	int n; /*valeur comprise en 0 et n_max. C'est le nombre de sommets du graphe*/
-} graphe_m;
+/**
+  * @typedef sommet
+  * @brief Représente un sommet par son indice.
+  */
+typedef int sommet;
 
-graphe_m gm;
-
-/*représentation par listes d'adjacence*/
+/**
+  * @struct chainon
+  * @brief Element d'une liste.
+  */
+/**
+  * @typedef Couple
+  * @brief Un élément et le pointeur vers l'élément suivant.
+  */
 typedef struct chainon
 {
-	sommet st; /*indice de sommet de 0 à n_max-1*/
-	int c;
-	int circ;
-	struct chainon *suivant;
+	sommet st; /**< Indice du sommet */
+	struct chainon *suivant; /**< Pointeur vers l'élément suivant de la liste. */
 } Couple;
 
+/**
+  * @typedef liste
+  * @brief Une liste est une suite de couple.
+  */
 typedef Couple *liste;
 
+/**
+  * @typedef graphe_l
+  * @brief Un graphe est un ensemble de n sommets, dont on liste les voisins.
+  */
 typedef struct
 {
-	liste a[n_max]; /*tableau de liste*/
-	int n; /*valeur comprise entre 0 et n_max. C'est le nombre de sommets du graphe*/
+	liste a[n_max]; /**< Tableau de voisins de chacun des sommets. */
+	int n; /**< Nombre de sommets du graphes. */
 } graphe_l;
 
-graphe_l gl;
+/**
+  * @typedef EnsembleDeSommets
+  * @brief Représente un ensemble de sommet via une liste chainée
+  */
+typedef liste EnsembleDeSommets;
 
+/**
+  * @fn graphe_l* createGraphe(int n);
+  * @brief Constructeur de graphe
+  *
+  * Alloue et initialise un graphe de n noeuds.
+  *
+  * @param[in] n Nombre de noeuds du graphe
+  * @return Un graphe de n noeuds.
+  */
+graphe_l* createGraphe(int n);
 
-typedef struct pfile
-{
-	sommet st;
-	struct pfile *suivant;
-} ppfile;
+/**
+  * @fn void initGraphe(graphe_l* g, int n);
+  * @brief Initialise un graphe alloué avec n noeud.
+  *
+  * @param g Graphe à initialiser
+  * @param n Nombre de noeud du graphe
+  */
+void initGraphe(graphe_l* g, int n);
 
-typedef ppfile *Pfile;
+/**
+  * @fn void releaseGraphe(graphe_l* g);
+  * @brief Libère la mémoire alloué par le graphe
+  *
+  * @param g Graphe à libérer
+  */
+void releaseGraphe(graphe_l* g);
 
-#endif
+/**
+  * @fn void destroyGraphe (graphe_l** g);
+  * @brief Désalloue et détruit un graphe
+  *
+  * @param g Pointeur sur le graphe à désalouer et à détruire
+  */
+void destroyGraphe (graphe_l** g);
+
+/**
+  * @fn graphe_l* loadFromFile (char* filename);
+  * @brief Crée et initialise un graphe depuis le fichier filename.
+  *
+  * @param filename Chemin et nom du fichier contenant la description du graphe
+  * @return Un nouveau graphe, alloué et initialisé
+  */
+graphe_l* loadFromFile (char* filename);
+
+/**
+  * @fn void saveToFile (graphe_l g, char* filename);
+  * @brief Sauvegarde le graphe g dans le fichier filename.
+  *
+  * @param g Graphe à sauvegarder
+  * @param filename chemin et nom du fichier résultant
+  */
+void saveToFile (graphe_l g, char* filename);
+
+/**
+  * @fn graphe_l* loadFromInput ();
+  * @brief Crée un graphe et l'initialise grace à une description saisie sur
+l'entrée standard.
+  *
+  * @return Le nouveau graphe créé
+  */
+graphe_l* loadFromInput ();
+
+/**
+  * @fn char* toString (graphe_l g);
+  * @brief Retroune une chaine de caractère représentant le graphe.
+  *
+  * @param g Le graphe à représenter
+  * @return Une chaine de caractère représentative à afficher
+  */
+char* toString (graphe_l g);
+
+/**
+  * @fn int existArc(graphe_l g, sommet s1, sommet s2);
+  * @brief Teste l'existance d'un arc dans le graphe
+  *
+  * @param g Graphe à vérfier
+  * @param s1 Premier sommet de l'arc
+  * @param s2 Second sommet de l'arc
+  * @return 1 si l'arc s1-s2 (ou s2-s1) existe, 1 sinon.
+  */
+int existArc (graphe_l g, sommet s1, sommet s2);
+
+/**
+  * @fn int isClique (graphe_l g, EnsembleDeSommets E);
+  * @brief Teste si E est une clique de g.
+  *
+  * @param g Graphe
+  * @param E Ensemble de sommet à tester
+  * @return 1 si E est une clique de g, 0 sinon.
+  */
+int isClique (graphe_l g, EnsembleDeSommets E);
+
+#endif /* __GRAPHE_H__ */
