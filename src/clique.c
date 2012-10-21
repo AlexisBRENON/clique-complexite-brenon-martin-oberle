9c533f98ef2bea2f1f180a6882fdd350e1112684
brenon.alexis <brenon.alexis@0f5846b4-0016-f348-011b-61e5118c5e03>
2012-10-21 14:14:00 +0000

    Projet fontionnel, reste à commenter.

--- src/clique.c
+++ src/clique.c
@@ -4,6 +4,7 @@
 #include "graphe.h"
 #include "clique.h"
 
+
 //O(n^3)
 //passer e en liste chainee (pb 2eme boucle??)
 /*int verif_clique(graphe_l g, int e[]){
@@ -31,7 +32,7 @@
 
 //O(n²)
 //passer e en liste chainee (pb 2eme boucle??)
-int isClique(graphe_l g, EnsembleDeSommet e){
+int isClique(graphe_l g, EnsembleDeSommets e){
 	sommet x,i;
 	liste p;
 	int ok;
@@ -64,8 +65,7 @@ int isClique(graphe_l g, EnsembleDeSommet e){
 }
 
 //O(n²)
-//passer e en liste chainee (pb 2eme boucle??)
-int isMaximale(graphe_l g, ensembleDeSommet e)
+int isMaximale(graphe_l g, EnsembleDeSommets e)
 {
 	sommet x,i;
 	liste p;
@@ -102,10 +102,14 @@ int isMaximale(graphe_l g, ensembleDeSommet e)
 }
 
 //O(n²)
-//passer e en liste chainee (pb 2eme boucle??)
-ensembleDeSommet* getCliqueMaximale(graphe_l g)
+EnsembleDeSommets getCliqueMaximale(graphe_l g)
 {
-	ensembleDeSommet* e = (ensembleDeSommet*) malloc(sizeof(int)*g.n);
+	return getCliqueMaximaleFromSt(g, 0);
+}
+
+EnsembleDeSommets getCliqueMaximaleFromSt(graphe_l g, sommet st)
+{
+	EnsembleDeSommets e = (EnsembleDeSommets) malloc(sizeof(int)*g.n);
 	sommet x,i;
 	liste p;
 	int ok;
@@ -114,8 +118,8 @@ ensembleDeSommet* getCliqueMaximale(graphe_l g)
 	{
 		e[x]=0;
 	}
-	e[0]=1;
-	x=1;
+	e[st] = 1;
+	x = (st+1)%g.n;
 	do{
 		ok=1;
 		if (!e[x])
@@ -128,7 +132,8 @@ ensembleDeSommet* getCliqueMaximale(graphe_l g)
 				}
 				p=p->suivant;
 			}
-			for (i=0; i<g.n;i++)
+			i = x;
+			do
 			{
 				if (e[i]==1)
 				{
@@ -138,12 +143,155 @@ ensembleDeSommet* getCliqueMaximale(graphe_l g)
 				{
 					e[i]=1;
 				}
-			}
+                i = (i+1)%g.n;
+			} while (i != x);
 		}
 
 		if (ok)
 			e[x]=1;
-		x++;
-	}while(x<g.n && !ok);
+		x = (x+1)%g.n;
+	} while(x!=st);
 	return e;
+}
+
+EnsembleDeSommets getCliqueMaximumInc (graphe_l g)
+{
+	/* Résultat des appels à getCliqueMaximum */
+	EnsembleDeSommets e = NULL;
+	/* Stocke les résultats indexés par leur taille */
+	EnsembleDeSommets* results =
+		(EnsembleDeSommets*) malloc(sizeof(EnsembleDeSommets)*g.n);
+	/* Sommet déja inclus dans une clique et donc à ne pas re-tester */
+	EnsembleDeSommets done =
+		(EnsembleDeSommets) malloc(sizeof(int)*g.n);
+
+	int i = 0;
+	int loop = 1;
+	sommet st = 0;
+	int ok = 0;
+
+	for (i = 0; i < g.n; i++)
+	{
+        results[i] = NULL;
+		done[i] = 0;
+	}
+
+	while (ok == 0)
+	{
+		e = getCliqueMaximaleFromSt(g, st);
+		int counter = 0;
+		/* On compte le nombre de sommets présents */
+		for (i = 0; i < g.n; i++)
+		{
+			if (e[i] == 1)
+			{
+				done[i] = 1;
+				counter++;
+			}
+		}
+		/* On sauvegarde la clique actuelle */
+		if (results[counter] != NULL)
+		{
+		    free(results[counter]);
+		}
+		results[counter] = e;
+
+		loop = 1;
+		i = 0;
+		while (i < g.n && loop == 1) {
+			if (done[i] == 0)
+			{
+				st = i;
+				loop = 0;
+			}
+			i++;
+		}
+		if (loop == 1)
+		{
+			ok = 1;
+		}
+	}
+
+	/* Désallocation de la mémoire et renvoi du resultat. */
+	free(done);
+	i = g.n-1;
+	while (i >= 0)
+	{
+		if (results[i] != NULL && loop == 1)
+		{
+			e = results[i];
+			loop = 0;
+		}
+		else if (results[i] != NULL)
+		{
+			free(results[i]);
+		}
+		i--;
+	}
+	free(results);
+
+	return e;
+}
+
+void getCliqueMaximumFullRec (graphe_l g,
+                                EnsembleDeSommets cliqueToTest,
+                                int size,
+                                int currentVertex,
+                                EnsembleDeSommets cliqueMax,
+                                int* maxSize)
+{
+    sommet x;
+
+    if (isClique(g, cliqueToTest) == 1)
+    {
+        if (size > *maxSize)
+        {
+            for (x = 0; x < g.n; x++)
+            {
+                cliqueMax[x] = cliqueToTest[x];
+            }
+            *maxSize = size;
+        }
+    }
+    else if (currentVertex < g.n)
+    {
+        x = currentVertex;
+        while (x < g.n)
+        {
+            cliqueToTest[x] = 0;
+            getCliqueMaximumFullRec(g,
+                                    cliqueToTest,
+                                    size-1,
+                                    x+1,
+                                    cliqueMax,
+                                    maxSize);
+            cliqueToTest[x] = 1;
+            x++;
+        }
+    }
+}
+
+EnsembleDeSommets getCliqueMaximumFull (graphe_l g)
+{
+	EnsembleDeSommets cliqueToTest =
+		(EnsembleDeSommets) malloc(sizeof(int)*g.n);
+    EnsembleDeSommets cliqueMax =
+		(EnsembleDeSommets) malloc(sizeof(int)*g.n);
+	int i = 0;
+	for (i = 0; i < g.n; i++)
+	{
+		cliqueToTest[i] = 1;
+		cliqueMax[i] = 0;
+	}
+
+    int maxSize = 0;
+	getCliqueMaximumFullRec(
+                        g, /* Graphe à analyser */
+                        cliqueToTest,
+                        g.n,
+                        0,
+                        cliqueMax,
+                        &maxSize);
+    free(cliqueToTest);
+	return cliqueMax;
 }
--- src/main.c
+++ src/main.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 
 #include "graphe.h"
+#include "clique.h"
 
 int main (int argc, char* argv[])
 {
@@ -14,7 +15,90 @@ int main (int argc, char* argv[])
     }
 
     graphe_l *g = loadFromFile(argv[1]);
+    if (g == NULL) {return EXIT_FAILURE;}
     dispGraphe(*g);
 
+    int loop = 1;
+    int choice = 0;
+    EnsembleDeSommets e = NULL;
+    int i = 0;
+    int starting = 0;
+
+	while (loop == 1)
+	{
+		printf("Que voulez-vous faire ?\n");
+		printf("\t1 : Clique Maximale\n");
+		printf("\t2 : Clique Maximale Contenant...\n");
+		printf("\t3 : Clique Maximum (calcul incomplet)\n");
+		printf("\t4 : Clique Maximum (calcul complet)\n");
+		printf("\t9 : Quitter\n");
+		printf("  -> ");
+		scanf("%d", &choice);
+		switch (choice)
+		{
+			case 1:
+				e = getCliqueMaximale(*g);
+				printf ("Une clique maximale est composée de :\n\t{ ");
+				for (i = 0; i < g->n; i++)
+				{
+					if (e[i] == 1)
+					{
+						printf("%d ", i);
+					}
+				}
+				printf("}\n");
+				free(e);
+				break;
+			case 2:
+                printf("Quel sommet ?\n  -> ");
+                scanf("%d", &starting);
+				e = getCliqueMaximaleFromSt(*g, starting);
+				printf ("Une clique maximale est composée de :\n\t{ ");
+				for (i = 0; i < g->n; i++)
+				{
+					if (e[i] == 1)
+					{
+						printf("%d ", i);
+					}
+				}
+				printf("}\n");
+				free(e);
+				break;
+			case 3:
+				e = getCliqueMaximumInc(*g);
+				printf ("La clique maximum est supposée être :\n\t{ ");
+				for (i = 0; i < g->n; i++)
+				{
+					if (e[i] == 1)
+					{
+						printf("%d ", i);
+					}
+				}
+				printf("}\n");
+				free(e);
+				break;
+			case 4:
+				e = getCliqueMaximumFull(*g);
+				printf ("La clique maximum est :\n\t{ ");
+				for (i = 0; i < g->n; i++)
+				{
+					if (e[i] == 1)
+					{
+						printf("%d ", i);
+					}
+				}
+				printf("}\n");
+				free(e);
+				break;
+			case 9:
+				loop = 0;
+				printf("Au revoir\n");
+				break;
+			default:
+				break;
+		}
+	}
+
+    destroyGraphe(&g);
     return EXIT_SUCCESS;
 }
